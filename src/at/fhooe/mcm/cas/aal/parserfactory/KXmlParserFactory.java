f420eb57f7a185bfd200c0c95c7235ca4cd0783a
Oliver Barth <o.barth19@gmail.com>
2017-11-15 15:23:53 +0100

    returning right parser

--- src/at/fhooe/mcm/cas/aal/parserfactory/KXmlParserFactory.java
+++ src/at/fhooe/mcm/cas/aal/parserfactory/KXmlParserFactory.java
@@ -1,13 +1,13 @@
 package at.fhooe.mcm.cas.aal.parserfactory;
 
-import at.fhooe.mcm.cas.aal.parser.DOMContextParser;
 import at.fhooe.mcm.cas.aal.parser.IContextParser;
+import at.fhooe.mcm.cas.aal.parser.KXmlContextParser;
 
 public class KXmlParserFactory implements IParserFactory {
 
 	@Override
 	public IContextParser createParser() {
-		return new DOMContextParser();
+		return new KXmlContextParser();
 	}
 
 }
