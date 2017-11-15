ffc6705c56c7cd90e487b318e22c366a8e563885
Oliver Barth <o.barth19@gmail.com>
2017-11-15 15:27:24 +0100

    adding class with constants for all elements to parse

---
+++ src/at/fhooe/mcm/cas/contexttype/ContextElementType.java
@@ -0,0 +1,43 @@
+package at.fhooe.mcm.cas.contexttype;
+
+public class ContextElementType {
+	public static final String START_TAG = "contextElement";
+	public static final String CONTEXT_ID = "contextId";
+	public static final String CONTEXT_KEY = "contextKey";
+	public static final String CONTEXT_VALUE = "contextValue";
+	
+	public static final String CONTEXT_ELEMENT_FUEL = "fuel";
+	public static final String CONTEXT_ELEMENT_POSITION = "position";
+	public static final String CONTEXT_ELEMENT_TEMPERATURE = "temperature";
+	public static final String CONTEXT_ELEMENT_TIME_OF_THE_DAY = "timeOfTheDay";
+	
+	
+	public class ContextElementFuel {
+		public static final String FUEL_STATUS = "fuelStatus";
+	}
+	
+	public class ContextElementPosition {
+		public static final String COORDINATE_TYPE = "coordinateType";
+		public static final String X_VALUE = "xValue";
+		public static final String Y_VALUE = "yValue";
+	}
+	
+	public class ContextElementTemperature {
+		public static final String TEMPERATURE = "temperature";
+	}
+	
+	public class ContextElementTimeOfTheDay {
+		public static final String TIME = "time";
+		public static final String DATE = "date";
+		
+		public class Time {
+			public static final String HOUR = "hour";
+			public static final String MINUTES = "minuts";
+		}
+		public class Date {
+			public static final String YEAR = "year";
+			public static final String MONTH = "month";
+			public static final String DAY = "day";
+		}
+	}
+}
