ab778829386f8ba1a06d2d7d70ec02a41783a00d
Oliver Barth <o.barth19@gmail.com>
2017-11-18 09:32:29 +0100

    AALComponent inits model, view, controller gets notified from AALModel about context elements notifies mediator

--- src/at/fhooe/mcm/cas/AALComponent.java
+++ src/at/fhooe/mcm/cas/AALComponent.java
@@ -1,29 +1,19 @@
 package at.fhooe.mcm.cas;
 
 import java.awt.Panel;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
-import at.fhooe.mcm.cas.aal.parser.IContextParser;
-import at.fhooe.mcm.cas.aal.parserfactory.DOMParserFactory;
-import at.fhooe.mcm.cas.aal.parserfactory.GsonParserFactory;
-import at.fhooe.mcm.cas.aal.parserfactory.IParserFactory;
-import at.fhooe.mcm.cas.aal.parserfactory.KXmlParserFactory;
+import at.fhooe.mcm.cas.aal.AALController;
+import at.fhooe.mcm.cas.aal.AALModel;
+import at.fhooe.mcm.cas.aal.AALModelObserver;
+import at.fhooe.mcm.cas.aal.AALView;
+import at.fhooe.mcm.cas.aal.parser.ParserMode;
 import at.fhooe.mcm.cas.contexttype.ContextElement;
 import at.fhooe.mcm.cas.gis.geomodel.GeoObject;
 
-public class AALComponent extends IComponent {
+public class AALComponent extends IComponent implements AALModelObserver {
 
-	private IContextParser mParser;
+	private Panel mPanel;
 	
-	public enum ParserMode {
-		DOM, STREAM, JSON
-	}
 	
 	public AALComponent(IMediator mediator, String name) {
 		this(mediator, name, ParserMode.DOM);
@@ -32,56 +22,12 @@ public class AALComponent extends IComponent {
 	public AALComponent(IMediator mediator, String name,  ParserMode mode) {
 		super(mediator, name);
 		
-
-		
-		// create a concrete factory for producing a parser
-		IParserFactory factory = null;
-		switch (mode) {
-			case STREAM : factory = new KXmlParserFactory(); break;
-			case DOM : factory = new DOMParserFactory(); break;
-			case JSON : factory = new GsonParserFactory(); break;
-		default:
-			factory = new KXmlParserFactory();
-		} // switch mode
-		
-		
-		mParser = factory.createParser();
-		
-		
-		
-		readAllFiles();
-		
-	}
-
-	private void readAllFiles() {
-
-		List<ContextElement> elements = new ArrayList<>();
-		File folder = new File("files/XML");
-		File[] listOfFiles = folder.listFiles();
-
-		for (int i = 0; i < listOfFiles.length; i++) {
-			File f = listOfFiles[i];
-			if (f.isFile()) {
-
-				try {
-					System.out.println("Parsing file " + f.getName());
-					Path path = FileSystems.getDefault().getPath(f.getAbsolutePath());
-					String context = new String(Files.readAllBytes(path.toAbsolutePath()));
-					elements.addAll(mParser.parse(context));
-
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-			} else if (listOfFiles[i].isDirectory()) {
-				System.out.println("Directory " + f.getName());
-			}
-		}
-
-		// notify
-		for (ContextElement e : elements) {
-			mMediator.notifyComponents(e, this);
-		}
+		AALModel m = new AALModel();
+		m.addObserver(this);
+		m.setParserMode(mode);
+		AALController c = new AALController(m);
+		AALView v = new AALView(c);
+		mPanel = v.getPanel();
 
 	}
 
@@ -100,8 +46,7 @@ public class AALComponent extends IComponent {
 
 	@Override
 	public Panel getView() {
-		// TODO Auto-generated method stub
-		return null;
+		return mPanel;
 	}
 
 	@Override
@@ -114,6 +59,12 @@ public class AALComponent extends IComponent {
 	public void onGPSPositionUpdated(GPSPosition gpsPosition) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void update(ContextElement contextElement) {
+		// notify mediator
+		mMediator.notifyComponents(contextElement, this);
 	}
 
 }
