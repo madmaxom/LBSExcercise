9703d151e7c8c943ddc171fa81989822597312d1
Oliver Barth <o.barth19@gmail.com>
2017-11-19 15:41:59 +0100

    adding radio buttons for selecting xml or json files

--- src/at/fhooe/mcm/cas/aal/AALView.java
+++ src/at/fhooe/mcm/cas/aal/AALView.java
@@ -2,7 +2,13 @@ package at.fhooe.mcm.cas.aal;
 
 import java.awt.Button;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Panel;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class AALView {
 
@@ -12,6 +18,25 @@ public class AALView {
 	public AALView(AALController controller) {
 		
 		mController = controller;
+		
+		JPanel radioPanel = new JPanel(new GridLayout(0, 1));
+	    ButtonGroup group = new ButtonGroup();
+	    
+	    JRadioButton rbXml = new JRadioButton("Xml");
+	    rbXml.setActionCommand("Xml");
+	    rbXml.setSelected(true);
+	    rbXml.setName("rbXml");
+	    rbXml.addActionListener(mController);
+		group.add(rbXml);
+		radioPanel.add(rbXml);
+		
+	    JRadioButton rbJson = new JRadioButton("Json");
+	    rbJson.setActionCommand("Json");
+	    rbJson.setName("rbJson");
+	    rbJson.setSelected(false);
+	    rbJson.addActionListener(mController);
+		group.add(rbJson);
+		radioPanel.add(rbJson);
 		
 		Button btnLoadFileSystem = new Button("Load from FileSystem");
 		btnLoadFileSystem.setName("btnLoadFileSystem");
@@ -25,6 +50,7 @@ public class AALView {
 		mPanel = new Panel(new FlowLayout());
 		mPanel.add(btnLoadServer, FlowLayout.LEFT);
 		mPanel.add(btnLoadFileSystem, FlowLayout.LEFT);
+		mPanel.add(radioPanel, FlowLayout.LEFT);
 	}
 	
 
