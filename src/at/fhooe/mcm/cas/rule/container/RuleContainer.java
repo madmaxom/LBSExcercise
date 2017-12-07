63663f342effde71ad096766a05222ab21504978
Oliver Barth <o.barth19@gmail.com>
2017-12-07 19:07:32 +0100

    updated rule container logs

--- src/at/fhooe/mcm/cas/rule/container/RuleContainer.java
+++ src/at/fhooe/mcm/cas/rule/container/RuleContainer.java
@@ -7,6 +7,7 @@ import at.fhooe.mcm.cas.ContextSituation;
 import at.fhooe.mcm.cas.compiler.treenode.NodeError;
 import at.fhooe.mcm.cas.compiler.treenode.TreeNode;
 import at.fhooe.mcm.cas.contexttype.ContextElement;
+import at.fhooe.mcm.cas.contexttype.ContextFuel;
 import at.fhooe.mcm.cas.contexttype.ContextTemperature;
 import at.fhooe.mcm.cas.compiler.generated.*;
 import at.fhooe.mcm.cas.compiler.generated.Compiler;
@@ -14,8 +15,12 @@ import at.fhooe.mcm.cas.compiler.generated.Compiler;
 public class RuleContainer {
 	private TreeNode mConditionRoot = null;
 	private Action mAction = null;
+	private String mCondition;
 	
 	public RuleContainer(String _condition, Action _action) {
+		mCondition = _condition;
+		mAction = _action;
+		
 		InputStream input = new ByteArrayInputStream(_condition.getBytes());
 		Compiler compiler = new Compiler(input);
 		try {
@@ -23,7 +28,7 @@ public class RuleContainer {
 		} catch (ParseException e1) {
 			e1.printStackTrace();
 		}
-		mAction = _action;
+		
 	}
 	
 	public boolean valid(ContextSituation _sit) {
@@ -32,16 +37,40 @@ public class RuleContainer {
 		}
 		if(mConditionRoot != null) {
 			for(ContextElement ce : _sit.mContextElements) {
-				if(ce instanceof ContextTemperature) {
-					mConditionRoot.setVariableParameters(
-						new Object[] { ((ContextTemperature)ce).getTemperature()});
-					System.out.println(((ContextTemperature)ce).getTemperature());
-					try {
-						System.out.println(mConditionRoot.calculate().toString());
-					} catch (NodeError e) {
-						e.printStackTrace();
+				try {
+					Object[] obj = null;
+					
+					String valueStr = "";
+					if(ce instanceof ContextTemperature) {
+						Object value = ((ContextTemperature)ce).getTemperature();
+						obj = new Object[] { value };	
+						valueStr = value.toString();
+					} else if (ce instanceof ContextFuel) {
+						
+					} else {
+						// System.out.print("unknown context type");
+						return false;
 					}
+					
+					
+					System.out.println();
+					System.out.print("Evaluating: Rule: ");
+					System.out.print(mCondition);
+					System.out.print(", Value: ");
+					System.out.print(valueStr);
+					
+					
+					mConditionRoot.setVariableParameters(obj);
+					boolean valid = (boolean) mConditionRoot.calculate();
+					System.out.print(" -> " + valid);
+					
+					
+				} catch (NodeError e) {
+					e.printStackTrace();
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
+				
 			}
 		}
 		return true;
