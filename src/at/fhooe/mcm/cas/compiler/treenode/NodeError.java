bf21591bf3a7a981140f586b54f982231dffe43d
Oliver Barth <o.barth19@gmail.com>
2017-12-08 15:44:09 +0100

    support message for NodeError

--- src/at/fhooe/mcm/cas/compiler/treenode/NodeError.java
+++ src/at/fhooe/mcm/cas/compiler/treenode/NodeError.java
@@ -1,5 +1,18 @@
 package at.fhooe.mcm.cas.compiler.treenode;
 
 public class NodeError extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NodeError() {
+		this("");
+	}
+
+	public NodeError(String message) {
+		super(message);
+	}
 
 }
