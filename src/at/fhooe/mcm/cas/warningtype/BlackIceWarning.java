02c2e4981db6b9e2263684a8c73d32627ebb3274
Oliver Barth <o.barth19@gmail.com>
2017-12-09 11:59:09 +0100

    adding warning type classes

--- src/at/fhooe/mcm/cas/warningtype/BlackIceWarning.java
+++ src/at/fhooe/mcm/cas/warningtype/BlackIceWarning.java
@@ -1,5 +1,17 @@
 package at.fhooe.mcm.cas.warningtype;
 
-public class BlackIceWarning implements IWarningType {
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+public class BlackIceWarning extends IWarningType {
+
+	public BlackIceWarning() {
+		try {
+			image = ImageIO.read(new File("warning/black_ice.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/at/fhooe/mcm/cas/warningtype/FuelWarning.java
+++ src/at/fhooe/mcm/cas/warningtype/FuelWarning.java
@@ -1,5 +1,17 @@
 package at.fhooe.mcm.cas.warningtype;
 
-public class FuelWarning implements IWarningType {
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+public class FuelWarning extends IWarningType {
+	
+	public FuelWarning() {
+		try {
+			image = ImageIO.read(new File("warning/fuel.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
