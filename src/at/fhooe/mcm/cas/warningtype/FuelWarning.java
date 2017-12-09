c1295e6b386624361a2b1fef0e0c4389746705b7
Oliver Barth <o.barth19@gmail.com>
2017-12-09 10:15:20 +0100

    adding basic warning types

---
+++ src/at/fhooe/mcm/cas/warningtype/FuelWarning.java
@@ -0,0 +1,5 @@
+package at.fhooe.mcm.cas.warningtype;
+
+public class FuelWarning implements IWarningType {
+
+}
---
+++ src/at/fhooe/mcm/cas/warningtype/IWarningType.java
@@ -0,0 +1,5 @@
+package at.fhooe.mcm.cas.warningtype;
+
+public interface IWarningType {
+
+}
