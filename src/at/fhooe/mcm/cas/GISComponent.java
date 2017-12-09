32150cb11a4516438d71f2ddef23a10d2b98826d
Oliver Barth <o.barth19@gmail.com>
2017-12-09 10:16:34 +0100

    updating execute action code providing methods for actions

--- src/at/fhooe/mcm/cas/GISComponent.java
+++ src/at/fhooe/mcm/cas/GISComponent.java
@@ -11,6 +11,7 @@ import at.fhooe.mcm.cas.gis.GISView;
 import at.fhooe.mcm.cas.gis.geomodel.GeoObject;
 import at.fhooe.mcm.cas.rule.container.RuleContainer;
 import at.fhooe.mcm.cas.rule.container.RuleEvaluator;
+import at.fhooe.mcm.cas.warningtype.IWarningType;
 
 
 public class GISComponent extends IComponent {
@@ -83,12 +84,24 @@ public class GISComponent extends IComponent {
 		}
 		List<RuleContainer> rules = mRuleEvaluator.getRules();
 		for (RuleContainer r : rules) {
+			if (!r.getAction().getClazz().equals(this.getClass().getName())) {
+				// action not for that class, skip evaluation
+				continue;
+			}
 			boolean valid = r.valid(mContextSituation);
 			if (valid) {
-				r.execute();
+				r.execute(this);
 			}
 		}
 		
+	}
+	
+	public void setWarning(IWarningType warningType) {
+		System.out.println("Set warning with type: " + warningType.getClass().getSimpleName());
+	}
+	
+	public void setWarning(IWarningType warningType1, IWarningType warningType2) {
+		System.out.println("Set warning with type: " + warningType1.getClass().getSimpleName() + " and " + warningType2.getClass().getSimpleName());
 	}
 	
 
