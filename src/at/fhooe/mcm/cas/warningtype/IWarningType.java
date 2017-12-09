b89d5c4575cc631a774bb5b7d2fe10eedeb725be
Oliver Barth <o.barth19@gmail.com>
2017-12-09 11:58:59 +0100

    adding warning type base class

--- src/at/fhooe/mcm/cas/warningtype/IWarningType.java
+++ src/at/fhooe/mcm/cas/warningtype/IWarningType.java
@@ -1,5 +1,12 @@
 package at.fhooe.mcm.cas.warningtype;
 
-public interface IWarningType {
+import java.awt.image.BufferedImage;
 
+public abstract class IWarningType {
+	
+	protected BufferedImage image;
+	
+	public BufferedImage getBufferedImage() {
+		return image;
+	}
 }
