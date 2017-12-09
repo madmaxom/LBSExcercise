de9e185ca1b154c57fe4028b108c91c4ad0a593d
Oliver Barth <o.barth19@gmail.com>
2017-12-09 10:16:13 +0100

    adding getters adding execute method for actions using reflection api

--- src/at/fhooe/mcm/cas/rule/container/RuleContainer.java
+++ src/at/fhooe/mcm/cas/rule/container/RuleContainer.java
@@ -2,10 +2,13 @@ package at.fhooe.mcm.cas.rule.container;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 import at.fhooe.mcm.cas.ContextSituation;
+import at.fhooe.mcm.cas.IComponent;
 import at.fhooe.mcm.cas.compiler.treenode.NodeError;
 import at.fhooe.mcm.cas.compiler.treenode.TreeNode;
 import at.fhooe.mcm.cas.contexttype.ContextElement;
@@ -29,6 +32,14 @@ public class RuleContainer {
 			e1.printStackTrace();
 		}
 		
+	}
+	
+	public Action getAction() {
+		return mAction;
+	}
+	
+	public String getCondition() {
+		return mCondition;
 	}
 	
 	public boolean valid(ContextSituation _sit) {
@@ -84,7 +95,57 @@ public class RuleContainer {
 		return null;
 	}
 
-	public void execute() {
+	public void execute(IComponent comp) {
 		System.out.println(this.getClass().getSimpleName() + ": Execute action " + mAction.toString());
+		
+		try {
+			// Class to call method
+			Class c = comp.getClass();
+			if (!mAction.getClazz().equals(c.getName())) {
+				// action not defined for this class, return
+				return;
+			}
+			
+			// Construct parameter types and value objects
+			List<Parameter> paramList = mAction.getParameter();
+			Class[] param = new Class[paramList.size()];
+			Object[] values = new Object[paramList.size()];
+			for (int i = 0; i < paramList.size(); i++) {
+				Class p = Class.forName(paramList.get(i).getParameterType());
+				param[i] = p;
+				Class v = Class.forName(paramList.get(i).getParameterValue());
+				Object obj = v.newInstance();
+				values[i] = obj;
+			}
+			
+			// get and call method
+			Method m = c.getMethod(mAction.getMethod(), param);
+			m.invoke(comp, values);
+			
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
